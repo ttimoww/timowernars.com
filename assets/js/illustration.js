d6a750102294eaddce8698783fb5fce6cbc475b8
Timo <ttimoww@gmail.com>
2019-03-11 17:04:23 +0100

    Final design

--- assets/js/illustration.js
+++ assets/js/illustration.js
@@ -24,7 +24,7 @@ $('body').mousemove(function(event) {
     var x = (eye.offset().left) + (eye.width() / 2);
     var y = (eye.offset().top) + (eye.height() / 2);
     var rad = Math.atan2(event.pageX - x, event.pageY - y);
-    var rot = (rad * (180 / Math.PI) * -1) + 180;
+    var rot = (rad * (180 / Math.PI) * -1) + 100;
     eye.css({
       '-webkit-transform': 'rotate(' + rot + 'deg)',
       '-moz-transform': 'rotate(' + rot + 'deg)',
